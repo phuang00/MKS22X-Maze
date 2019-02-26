cd44429138986400627c249f0eb6786e23db7986
phuang00 <phuang00@stuy.edu>
2019-02-26 11:43:44 -0500

    finished readFile so that it reads the maze into an array of characters

--- Maze.java
+++ Maze.java
@@ -2,21 +2,30 @@ import java.util.*;
 import java.io.*;
 
 public class Maze{
-  public static void readFile(String name) throws FileNotFoundException{
+  public static char[][] readFile(String name) throws FileNotFoundException{
     File f = new File(name);
     Scanner in = new Scanner(f);
     ArrayList<String> tempLines = new ArrayList<>();
     while (in.hasNextLine()){
       tempLines.add(in.nextLine());
     }
-    for (int i = 0; i < tempLines.size(); i++){
-      System.out.println(tempLines.get(i));
+    int x = tempLines.size();
+    int y = tempLines.get(0).length();
+    char[][] ans = new char[x][y];
+    for (int i = 0; i < x; i++){
+      for (int j = 0; j < y; j++){
+        ans[i][j] = tempLines.get(i).charAt(j);
+      }
     }
+    return ans;
   }
 
   public static void main(String[] args) {
     try{
-      readFile("Maze1.txt");
+      char[][] ans = readFile("Maze1.txt");
+      for (int i = 0; i < ans.length; i++){
+        System.out.println(Arrays.toString(ans[i]));
+      }
     }catch(FileNotFoundException e){
       e.printStackTrace();
     }
